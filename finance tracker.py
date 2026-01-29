e4f1938d76ee4da65c3f22d51e820eb9e7e75fcf
hadi mahmoud <hadiamahmoud2005@gmail.com>
2026-01-29 13:50:33 +0000

    Update finance tracker.py

--- finance tracker.py
+++ finance tracker.py
@@ -1,11 +1,12 @@
-from database import create_table, add_transaction, get_transactions
+from database import create_table, add_transaction, get_transactions, get_monthly_summary
 
 def show_menu():
     print("\nPersonal Finance Tracker")
     print("1. Add Income")
     print("2. Add Expense")
     print("3. View Transactions")
-    print("4. Exit")
+    print("4. Monthly Summary")
+    print("5. Exit")
 
 def main():
     create_table()
@@ -17,13 +18,15 @@ def main():
         if choice == "1":
             category = input("Enter income category: ")
             amount = float(input("Enter amount: "))
-            add_transaction("Income", category, amount)
+            date = input("Enter date (YYYY-MM): ")
+            add_transaction("Income", category, amount, date)
             print("Income added successfully.")
 
         elif choice == "2":
             category = input("Enter expense category: ")
             amount = float(input("Enter amount: "))
-            add_transaction("Expense", category, amount)
+            date = input("Enter date (YYYY-MM): ")
+            add_transaction("Expense", category, amount, date)
             print("Expense added successfully.")
 
         elif choice == "3":
@@ -33,11 +36,28 @@ def main():
                 print(t)
 
         elif choice == "4":
+            month = input("Enter month (YYYY-MM): ")
+            summary = get_monthly_summary(month)
+
+            income = 0
+            expense = 0
+
+            for s in summary:
+                if s[0] == "Income":
+                    income = s[1]
+                elif s[0] == "Expense":
+                    expense = s[1]
+
+            balance = income - expense
+
+            print(f"\nSummary for {month}")
+            print(f"Total Income: £{income}")
+            print(f"Total Expenses: £{expense}")
+            print(f"Balance: £{balance}")
+
+        elif choice == "5":
             print("Goodbye!")
             break
 
         else:
             print("Invalid option. Try again.")
-
-if __name__ == "__main__":
-    main()
