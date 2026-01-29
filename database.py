46c43939adcf497b06c9c74c00a044afb5d7cd0a
hadi mahmoud <hadiamahmoud2005@gmail.com>
2026-01-29 13:44:15 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,36 @@
+mport sqlite3
 
+def connect_db():
+    return sqlite3.connect("finance.db")
+
+def create_table():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT NOT NULL,
+            category TEXT NOT NULL,
+            amount REAL NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def add_transaction(t_type, category, amount):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO transactions (type, category, amount) VALUES (?, ?, ?)",
+        (t_type, category, amount)
+    )
+    conn.commit()
+    conn.close()
+
+def get_transactions():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM transactions")
+    data = cursor.fetchall()
+    conn.close()
+    return data
